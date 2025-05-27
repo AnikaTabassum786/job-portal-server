c728bfa146da3d18697f7669c9d1f822609bae96
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-28 01:34:13 +0600

    Get all, get by Id done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 // respond with "hello world" when a GET request is made to the homepage
 
@@ -30,6 +30,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // Job Api
+        const jobCollection = client.db("job_portal").collection("jobs");
+
+        app.get('/jobs', async(req,res)=>{
+            const cursor = jobCollection.find()
+            const result =await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/jobs/:id', async(req,res)=>{
+            const id=req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result =await jobCollection.findOne(query)
+            res.send(result)
+        })
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -42,7 +60,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('Job portal is running')
 })
 
 app.listen(port, () => {
